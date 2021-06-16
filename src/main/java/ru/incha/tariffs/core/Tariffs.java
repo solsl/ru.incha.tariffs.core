f41f454ef491f9d0cd284bf120962cd4feb52307
Vyacheslav Soldatov <vyacheslav.soldatov@inbox.ru>
2021-06-16 13:40:38 +0300

    Initial commits

---
+++ src/main/java/ru/incha/tariffs/core/Tariffs.java
@@ -0,0 +1,19 @@
+/* *******************************************************************************
+ * Copyright (c) 2021 Vyacheslav Soldatov
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache Software License 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ ********************************************************************************/
+package ru.incha.tariffs.core;
+
+/**
+ * @author Vyacheslav Soldatov <vyacheslav.soldatov@inbox.ru>
+ *
+ */
+public class Tariffs {
+
+}
